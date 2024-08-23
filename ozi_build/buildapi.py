57e6e3fdac5498f5868a8400360011b9f9abb823
rjdbcm <rjdbcm@outlook.com>
2024-08-22 19:10:21 -0500

    OZI.build 1.2.2 - fix length check
    Signed-off-by: rjdbcm <rjdbcm@outlook.com>

--- ozi_build/buildapi.py
+++ ozi_build/buildapi.py
@@ -148,7 +148,7 @@ class Config:
         if any(i not in string.ascii_uppercase + string.ascii_lowercase + '-[],0123456789' for i in v):
             raise ValueError('pyproject.toml:project.optional-dependencies has invalid character in nested key "{}"'.format(k))
         for j in (name for name in v.strip('[]').split(',')):
-            if j[0] in string.ascii_uppercase + string.ascii_lowercase:
+            if len(j) > 0 and j[0] in string.ascii_uppercase + string.ascii_lowercase:
                 for package in self.__extras.get(j, []):
                     metadata += 'Requires-Dist: {}; extra=="{}"\n'.format(package, k)
             else:
